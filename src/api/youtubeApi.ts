f2692e15f99cc47f3e92c8dfce4fcaf5875eb3c2
pankaj yadav <yadavpankaj.tech@gmail.com>
2025-04-20 05:09:30 +0000

    update the api

--- src/api/youtubeApi.ts
+++ src/api/youtubeApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const YOUTUBE_API_KEY = 'AIzaSyB1fVKntZPLFZCSCm5u0VBmSkXgQqfA52A'; 
+const YOUTUBE_API_KEY = 'AIzaSyCHe74pDlOPPo_Tu51_frtInyAx5rEmOx0'; 
 const YOUTUBE_API_URL = 'https://www.googleapis.com/youtube/v3';
 
 export const searchYoutubeVideo = async (query: string): Promise<string | null> => {
